56208aafe9c5b3d751b47ec1a9b6a0da7b98f9e2
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-08 23:25:59 +0200

    corrected a typo which crushed the server

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const serverStatic = require('server-static')
+const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
 
 // serves files from dist directory which now contains the built website
-app.use('/', serverStatic(path.join(__dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 const port = process.env.PORT || 8080
 
