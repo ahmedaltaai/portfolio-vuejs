9644f3ab4e0afcc725e6f8d9325b0d47e63781d8
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-08 13:53:42 +0200

    error component which I've created to display if the API call fails

---
+++ src/components/ErrorComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+    <p>error</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorComponent'
+}
+</script>
+
+<style lang="scss" scoped></style>
