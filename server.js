6017a28c274cb534aafc45d104d69eaf35bdda80
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-09 13:34:30 +0200

    removed serve-static package from the project

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-// const serveStatic = require('serve-static')
 const path = require('path')
 const history = require('connect-history-api-fallback')
 
@@ -8,7 +7,6 @@ const app = express()
 // serves files from dist directory which now contains the built website
 const staticFileMiddleware = express.static(path.join(__dirname, '/dist'))
 
-// app.use('/', serveStatic(path.join(__dirname, '/dist')))
 app.use(staticFileMiddleware)
 
 app.use(
