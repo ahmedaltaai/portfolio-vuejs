2a5a764ba6b1ff54b648c79213bb3b3363fc1ea5
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-09 13:16:55 +0200

    switched from serve-static to the static function of express

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express')
-const serveStatic = require('serve-static')
+// const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
 
 // serves files from dist directory which now contains the built website
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.get('/projects', serveStatic(path.join(__dirname, '/dist')))
-app.get('/articles', serveStatic(path.join(__dirname, '/dist')))
+// app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/', express.static(path.join(__dirname, '/dist')))
 
 const port = process.env.PORT || 8080
 
