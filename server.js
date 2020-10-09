4e3982c5f2123019be208e5869d60623ed5fe153
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-09 13:29:32 +0200

    added history stuff to the server

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express')
 // const serveStatic = require('serve-static')
 const path = require('path')
+const history = require('connect-history-api-fallback')
 
 const app = express()
 
 // serves files from dist directory which now contains the built website
+const staticFileMiddleware = express.static(path.join(__dirname, '/dist'))
+
 // app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.use('/', express.static(path.join(__dirname, '/dist')))
+app.use(staticFileMiddleware)
+
+app.use(
+  history({
+    disableDotRule: true,
+    verbose: true
+  })
+)
+
+app.use(staticFileMiddleware)
+
+app.get('/', (req, res) => {
+  res.render(path.join(__dirname, '/dist'))
+})
 
 const port = process.env.PORT || 8080
 
