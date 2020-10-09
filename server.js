efcbb9073bd91d0bbef4fa8822b62001c06ed9fa
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-09 13:09:48 +0200

    added two more routes for testing

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 
 // serves files from dist directory which now contains the built website
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.get('/projects', serveStatic(path.join(__dirname, '/dist')))
+app.get('/articles', serveStatic(path.join(__dirname, '/dist')))
 
 const port = process.env.PORT || 8080
 
