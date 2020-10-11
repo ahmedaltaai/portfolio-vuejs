46b93c6a0f807de324cd945c0f60a02ccdba317d
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-12 00:09:08 +0200

    added heroku-ssl-redirect to force the site to use https

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const path = require('path')
 const history = require('connect-history-api-fallback')
-
+const forceSSL = require('heroku-ssl-redirect')
 const app = express()
 
 // serves files from dist directory which now contains the built website
 const staticFileMiddleware = express.static(path.join(__dirname, '/dist'))
 
 app.use(staticFileMiddleware)
-
+app.use(forceSSL)
 app.use(
   history({
     disableDotRule: true,
