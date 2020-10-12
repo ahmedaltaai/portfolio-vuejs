5deffc6d0610b39b467be426e7a66468003f6014
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-13 01:07:18 +0200

    PLEASE!!

--- willhaben/server.js
+++ willhaben/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const path = require('path')
+// const path = require('path')
 const willhaben = express()
 
 willhaben.get('/', (req, res) => {
-  res.render(express.static(path.join(__dirname, '/')))
+  // res.render(express.static(path.join(__dirname, '/')))
+  res.send('HELLO')
 })
 
 module.exports = { willhaben }
