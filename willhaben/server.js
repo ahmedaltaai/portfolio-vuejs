5f111b301653062150a0134a677be1d6022bf9a8
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-13 01:01:30 +0200

    what about now??

--- willhaben/server.js
+++ willhaben/server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const willhaben = express()
 
 willhaben.get('/', (req, res) => {
-  res.send(express.static(path.join(__dirname, '/')))
+  res.render(express.static(path.join(__dirname, '/')))
 })
 
 module.exports = { willhaben }
