a404281cd4b1d6cc67f14424b02ad42a9c4766ec
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-13 02:19:09 +0200

    removed path

--- willhaben/app.js
+++ willhaben/app.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const path = require('path')
+// const path = require('path')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send(express.static(path.join(__dirname, '/')))
+  res.render(express.static('/'))
   // res.status(200).send('HELLO THIS FINALLY WORKED!')
 })
 
