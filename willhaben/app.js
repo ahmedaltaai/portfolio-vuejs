c6bc41b308491dba756f0e69389fad7f40f6936a
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-13 02:35:08 +0200

    NOW it should work because I put the files into a directory

--- willhaben/app.js
+++ willhaben/app.js
@@ -1,11 +1,11 @@
 const express = require('express')
-// const path = require('path')
+const path = require('path')
 const app = express()
 
-app.use(express.static(__dirname, '/'))
+app.use(express.static(path.join(__dirname, './public')))
 
 app.get('/', (req, res) => {
-  res.render(__dirname, '/')
+  res.render(path.join(__dirname, './public'))
 })
 
 module.exports = app
