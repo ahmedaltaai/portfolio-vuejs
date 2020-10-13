43ff345cf7b00bfd01327908e0776f919a5a36da
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-13 02:29:53 +0200

    crossing my fingers...

--- willhaben/app.js
+++ willhaben/app.js
@@ -2,9 +2,10 @@ const express = require('express')
 // const path = require('path')
 const app = express()
 
+app.use(express.static(__dirname, '/'))
+
 app.get('/', (req, res) => {
-  res.render(express.static('/'))
-  // res.status(200).send('HELLO THIS FINALLY WORKED!')
+  res.render(__dirname, '/')
 })
 
 module.exports = app
