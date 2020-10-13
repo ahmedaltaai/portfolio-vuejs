828a442ada090ca14b49daaec474317b2d311906
ahmedaltaai <altaai.ahmed@gmx.at>
2020-10-13 02:10:53 +0200

    rendering static with res.send instead of res.render

--- willhaben/app.js
+++ willhaben/app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.render(express.static(path.join(__dirname, '/')))
+  res.send(express.static(path.join(__dirname, '/')))
   // res.status(200).send('HELLO THIS FINALLY WORKED!')
 })
 
